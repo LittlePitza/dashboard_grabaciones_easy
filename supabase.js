5b0755905bf761ab07fe69dcc3271885fa386bd1
Luis Eduardo Hernandez Cruz <lhernandez025cz@gmail.com>
2026-05-20 13:46:12 -0600

    bux fix

--- supabase.js
+++ supabase.js
@@ -1,4 +1,3 @@
-// Rellena SUPABASE_URL y SUPABASE_ANON_KEY
 const SUPABASE_URL = 'https://fpwttcyemwqfkjsvwxwz.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_izZUskrc3yT49khe14SptQ__YX9MGk3';
-window.sb = (typeof supabase!=='undefined' && SUPABASE_URL.indexOf('TU_')===-1) ? supabase.createClient(SUPABASE_URL,SUPABASE_ANON_KEY) : null;
+window.supabaseClient = (SUPABASE_URL.includes('TU_') || SUPABASE_ANON_KEY.includes('TU_')) ? null : window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
